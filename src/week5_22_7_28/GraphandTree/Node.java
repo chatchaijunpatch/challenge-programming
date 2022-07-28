a05cc7249149cbb1ecf00bee6934d99bec499e7c
chatchai junpatch <chatchai.ju@ku.th>
2022-07-29 00:17:10 +0700

    update week 3 and 5

---
+++ src/week5_22_7_28/GraphandTree/Node.java
@@ -0,0 +1,33 @@
+package week5_22_7_28.GraphandTree;
+
+public class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value, Node... n) {
+        this.value = value;
+        left = null;
+        right = null;
+        if (n.length == 1)
+            left = n[0];
+        if (n.length == 2) {
+            left = n[0];
+            right = n[1];
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+
+}
